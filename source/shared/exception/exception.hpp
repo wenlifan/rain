cfd8688693c3b8bb93bc74ad01bd642cca3303a1
mogucpp <mogucpp@163.com>
2015-05-25 18:44:52 +0800

    add rain exception

---
+++ source/shared/exception/exception.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <string>
+#include <exception>
+
+namespace Rn
+{
+
+class RainException : public std::exception
+{
+public:
+    RainException(
+            std::string what,
+            std::string type = "",
+            const char *filename = nullptr,
+            int linenum = -1)
+    {
+        mWhat = "[Exception Occured]\n";
+
+        if (type.empty())
+            type = "UnKnow";
+
+        mWhat = mWhat + "Type:\t" + std::move(type) + '\n';
+        mWhat = mWhat + "What:\t" + std::move(what) + '\n';
+
+        if (filename != nullptr)
+        {
+            mWhat = mWhat + "Locate:\tLine "
+                    + std::to_string(linenum)
+                    + " in " + filename + '\n';
+        }
+    }
+
+    virtual const char * what() const noexcept override
+    {
+        return mWhat.c_str();
+    }
+
+private:
+    std::string mWhat;
+};
+
+}
+
+#define Throw(name, what) \
+    throw name(what, #name, __FILE__, __LINE__)
+
+#define DefineException(name) \
+class name : public Rn::RainException \
+{ \
+public: \
+    name(std::string what, \
+         std::string type, \
+         const char *filename, \
+         int linenum) \
+        : Rn::RainException(std::move(what), std::move(type), filename, linenum) \
+    {} \
+}
+
