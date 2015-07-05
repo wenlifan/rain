601293d08a2b05ff87f4a243586601905f82d102
mogucpp <mogucpp@163.com>
2015-07-05 16:56:08 +0800

    test

--- src/app/GameServer/main.cpp
+++ src/app/GameServer/main.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
+#include <chrono>
 
+#include "asio.hpp"
 #include "filesystem.hpp"
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Command: " << argv[0] << std::endl;
-    char buf[PATH_MAX];
-    getcwd(buf, PATH_MAX);
-    std::cout << "Current Work Dir: " << buf << std::endl;
     rain::resetWorkDir(argv[0]);
-    getcwd(buf, PATH_MAX);
-    std::cout << "Current Work Dir: " << buf << std::endl;
 
     auto vec = rain::getSortedDirFiles(".");
     for (auto &v : vec)
-    {
         std::cout << v << std::endl;
-    }
 
     std::cout << "Hello, World!\n";
 }
--- src/app/GateWay/main.cpp
+++ src/app/GateWay/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "asio.hpp"
+
 int main(void)
 {
     std::cout << "Hello, World!\n";
