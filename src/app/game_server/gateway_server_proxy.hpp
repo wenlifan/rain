4759b31491f84044037bf2bdadcb00c5bf980370
mogucpp <mogucpp@163.com>
2015-07-25 23:32:52 +0800

    .

--- src/app/game_server/gateway_server_proxy.hpp
+++ src/app/game_server/gateway_server_proxy.hpp
@@ -14,6 +14,11 @@ class GatewayProxy
     friend Singleton<GatewayProxy>;
     GatewayProxy() = default;
 
+    using TargetSession = Session<GatewayProxy>;
+    using TargetSessionPtr = std::shared_ptr<TargetSession>;
+    using Proxy = ClientProxy<GatewayProxy>;
+    using ProxyPtr = std::unique_ptr<Proxy>;
+
 public:
     void add_session(TargetSessionPtr session)
     {
